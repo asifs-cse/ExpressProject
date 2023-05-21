30a81819c375fa141fd005830aa797dfd5daed5f
unknown <asifs.cse@gmail.com>
2023-05-21 23:51:18 +0530

    express generator

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-//create route
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get((req, res)=>{
-  res.render('url not found!');
-});
-
-router.get('/about',(req, res)=>{
-  res.render('About section!');
-});
-
-router.get('/news',(req, res)=>{
-  res.render('News section!');
-});
-
-router.get('/signup',(req, res)=>{
-  res.render('Signup section');
-});
-
-router.get('/login',(req, res)=>{
-  res.render('login section');
-});
-
-//admin rote
-router.get('/admin',(req, res)=>{
-  res.render('admin activity');
-});
-
-//contact router
-
-router.get('/contact',(req, res)=>{
-  res.render('contact activity');
 });
 
 module.exports = router;
